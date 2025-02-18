287df6a7f21c964fc4cd08b9d438911be71a076f
Ijas Moopan <ijasmoopan46@gmail.com>
2025-02-18 11:41:57 +0530

    fix: chore next.config

--- next.config.mjs
+++ next.config.mjs
@@ -17,8 +17,13 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96],
     minimumCacheTTL: 60,
+    unoptimized: true,
   },
   webpack: (config, { dev, isServer }) => {
+    config.cache = {
+      type: "filesystem",
+      maxAge: 60 * 60 * 1000,
+    };
     // Enable tree shaking
     config.optimization.usedExports = true;
     config.optimization.minimize = true;
@@ -28,35 +33,31 @@ const nextConfig = {
         threshold: 10240, // Compress files > 10KB
       })
     );
+    config.optimization.minimize = true;
+    config.optimization.moduleIds = "deterministic";
 
-    // Add production-only optimizations
-    if (!dev) {
-      config.optimization.minimize = true;
-      config.optimization.moduleIds = "deterministic";
-
-      // Split chunks optimization
-      config.optimization.splitChunks = {
-        chunks: "all",
-        minSize: 10000,
-        minRemainingSize: 0,
-        minChunks: 1,
-        maxAsyncRequests: 30,
-        maxInitialRequests: 30,
-        enforceSizeThreshold: 50000,
-        cacheGroups: {
-          defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
+    // Split chunks optimization
+    config.optimization.splitChunks = {
+      chunks: "all",
+      minSize: 10000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
         },
-      };
-    }
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    };
     return config;
   },
 };
