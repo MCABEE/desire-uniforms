9248f649ec2a537508defe3bb0332fa66e796d4e
Ijas Moopan <ijasmoopan46@gmail.com>
2025-02-18 10:38:13 +0530

    fix: reduce build size

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,62 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  compress: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+
+  // Optimize images
   images: {
-    unoptimized: true, // Required for Next.js Image component in static export
+    domains: ["desireuniforms.in"],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    minimumCacheTTL: 60,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Enable tree shaking
+    config.optimization.usedExports = true;
+
+    config.optimization.minimize = true;
+
+    // Add production-only optimizations
+    if (!dev) {
+      config.optimization.minimize = true;
+      config.optimization.moduleIds = "deterministic";
+
+      // Split chunks optimization
+      config.optimization.splitChunks = {
+        chunks: "all",
+        minSize: 20000,
+        minRemainingSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      };
+    }
+    return config;
+  },
+  experimental: {
+    optimizeCss: true,
+    turbo: true,
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
